fd91a7016e85fce7cb9d7fcbaacf485fa18966db
Vardan <vardan.vanyan@gmail.com>
2024-08-26 17:45:48 -0700

    created window componenet

--- app/components/Desktop.tsx
+++ app/components/Desktop.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import MenuBar from './MenuBar';
 import Dock from './Dock';
+import Window from './Window';
 
 export default function Desktop() {
   return (
     <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
       <MenuBar />
-
+      <Window title='Vardan'>
+        <div>{'Vardan'}</div>
+      </Window>
       <Dock />
     </div>
   );
--- app/components/Window.tsx
+++ app/components/Window.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-export default function Window({ children }: { children: React.ReactNode }) {
-  return <div>
-    {children}
-    </div>;
+export default function Window({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='relative max-w-lg mx-auto my-8 border border-gray-300 rounded-lg shadow-lg overflow-hidden bg-white'>
+      {/* Title Bar */}
+      <div className='flex items-center p-2 bg-gray-100 border-b border-gray-300'>
+        {/* Traffic Lights */}
+        <div className='flex space-x-1 ml-1'>
+          <div
+            className='w-3 h-3 bg-red-500 rounded-full cursor-pointer'
+            title='Close'
+          ></div>
+          <div
+            className='w-3 h-3 bg-yellow-500 rounded-full cursor-pointer'
+            title='Minimize'
+          ></div>
+          <div
+            className='w-3 h-3 bg-green-500 rounded-full cursor-pointer'
+            title='Zoom'
+          ></div>
+        </div>
+        {/* Title */}
+        <div className='flex-grow text-center text-gray-600 font-medium ml-2'>
+          {title}
+        </div>
+      </div>
+
+      {/* Content Area */}
+      <div className='p-4'>{children}</div>
+    </div>
+  );
 }
